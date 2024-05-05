8d4ffda15a53ef5e716fa4f7b0411b57fea5d7c5
Simon Brown <simonandon@live.com.au>
2024-05-05 11:58:12 +0800

    Wrote sql script to set up db and tables

---
+++ db/schema.sql
@@ -0,0 +1,28 @@
+DROP DATABASE IF EXISTS employees_db;
+CREATE DATABASE employees_db;
+
+\c employees_db;
+
+CREATE TABLE department (
+	id SERIAL PRIMARY KEY,
+	name VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE role (
+	id SERIAL PRIMARY KEY,
+	title VARCHAR(30) NOT NULL,
+	salary DECIMAL NOT NULL,
+	department INTEGER,
+	FOREIGN KEY (department)
+	REFERENCES department(id)
+	ON DELETE SET NULL
+);
+
+CREATE TABLE employee (
+	id SERIAL PRIMARY KEY,
+	fist_name VARCHAR(30) NOT NULL,
+	last_name VARCHAR(30) NOT NULL,
+	role_id INTEGER,
+	FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE SET NULL,
+	manager_id INTEGER REFERENCES employee(id) ON DELETE SET NULL
+);
