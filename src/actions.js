459d0022173adfa068972a51ef99864a877b61f7
Simon Brown <simonandon@live.com.au>
2024-05-07 16:16:15 +0800

    Installed npm package console.table to print query results in a nice table. Added some comments

--- src/actions.js
+++ src/actions.js
@@ -1,17 +1,15 @@
 const inquirer = require('inquirer');
 const pool = require('./db');
 const queries = require('./queries');
+const cTable = require('console.table');
 
-const viewDepartments = function () {
-	pool.query(queries.viewDepartments, (err, result) => {
-		if (err) {
-			console.error(err);
-		}
-
-		console.log(result.rows);
-	});
+// function to print all departments
+const viewDepartments = async function () {
+	const result = await pool.query(queries.viewDepartments);
+	console.table(result.rows);
 };
 
+// export each action function
 module.exports = {
 	viewDepartments,
 }
